721717ff28870600e9000396571b7a73a6eab3b3
//yk <yashk.23@gmail.com>
2015-05-09 23:14:05 -0400

    Showing type, permissions, name for all dirent in dir

--- lab1/starter/ls/myls.c
+++ lab1/starter/ls/myls.c
@@ -6,48 +6,61 @@
  */
 
 #include <sys/types.h>
+#include <dirent.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>          // for printf().  man 3 printf
+#include <stdlib.h>         // for exit().    man 3 exit
+#include <pwd.h>            // for getpwuid()
+#include <grp.h>            // for getgrgid()
+#include <time.h>           // for ctime()
+#include <string.h>         // for strlen()
 
+#define STRLEN1 128
+#define STRLEN2 64
 int main(int argc, char *argv[])
 {
 	int i;
-	char str[] = "----------\0";
+	char type;
+	char perms[] = "---------\0";
 	struct stat buf;
+	DIR *p_dir;
+        struct dirent *p_dirent;
+       	if ((p_dir = opendir(argv[1])) == NULL) {
+        	printf("opendir(%s) failed\n", argv[1]);
+                exit(1);
+        }
 
-        for (i = 1; i < argc; i++) {
-                if (lstat(argv[i], &buf) < 0) {
-                        perror("lstat error");
-                        continue;
-                }   
+        while ((p_dirent = readdir(p_dir)) != NULL) {
+                char *str_path = p_dirent->d_name;      // relative path name!
+		//TODO: better error handling needed
+               	if (str_path == NULL) {
+              		printf("Null pointer found!");
+                        exit(2);
+               	} else {
+			if (lstat(str_path, &buf) < 0) {
+                        	perror("lstat error");
+                        	continue;
+               		}
+                        mode_t mode = buf.st_mode;
 
-		if (lstat(argv[i], &buf) < 0) {
-			perror("lstat error");
-			continue;
-		}   
-
-		mode_t mode = buf.st_mode;
-
-                if      (S_ISREG(mode))  str[0] = '-';
-                else if (S_ISDIR(mode))  str[0] = 'd';
+               		if      (S_ISREG(mode))  type = '-';
+                	else if (S_ISDIR(mode))  type = 'd';
 #ifdef S_ISLNK
-                else if (S_ISLNK(mode))  str[0] = 'l';
+                	else if (S_ISLNK(mode))  type = 'l';
 #endif
-                else                     str[0] = '?';
-		str[1] = (mode & S_IRUSR) ? 'r' : '-';
-		str[2] = (mode & S_IWUSR) ? 'w' : '-';
-		str[3] = (mode & S_IXUSR) ? 'x' : '-';	// assume no sticky bit
-		str[4] = (mode & S_IRGRP) ? 'r' : '-'; 
-		str[5] = (mode & S_IWGRP) ? 'w' : '-'; 
-		str[6] = (mode & S_IXGRP) ? 'x' : '-';
-		str[7] = (mode & S_IROTH) ? 'r' : '-'; 
-		str[8] = (mode & S_IWOTH) ? 'w' : '-'; 
-		str[9] = (mode & S_IXOTH) ? 'x' : '-';
-
-		printf("%s\n", str);
-	}
-
+                	else                     type = '?';
+			perms[0] = (mode & S_IRUSR) ? 'r' : '-';
+			perms[1] = (mode & S_IWUSR) ? 'w' : '-';
+			perms[2] = (mode & S_IXUSR) ? 'x' : '-';	// assume no sticky bit
+			perms[3] = (mode & S_IRGRP) ? 'r' : '-'; 
+			perms[4] = (mode & S_IWGRP) ? 'w' : '-'; 
+			perms[5] = (mode & S_IXGRP) ? 'x' : '-';
+			perms[6] = (mode & S_IROTH) ? 'r' : '-'; 
+			perms[7] = (mode & S_IWOTH) ? 'w' : '-'; 
+			perms[8] = (mode & S_IXOTH) ? 'x' : '-';
+			printf("%c%s %s\n", type, perms, str_path);
+                }
+        }
 	return 0;
 }
